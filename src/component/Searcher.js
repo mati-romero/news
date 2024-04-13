a9a92a889283cdc96e72debe1a199451f5789317
mati romero <matias.1993.romero@gmail.com>
2024-04-13 16:35:06 -0300

    NEWS-1802 - INPUT BUSCARDOR

--- src/component/Searcher.js
+++ src/component/Searcher.js
@@ -14,25 +14,40 @@ class Searcher extends React.Component {
 
     searchNews = async () => {
 
-        const search = this.state.search;
+        const search = this.state.search.trim();
+        const alertCtrlElement = document.getElementById('alertCtrl');
 
-        if(search != '') {
-            const token = '4588013a57a1fc2bb9ef42f2bec8f459';
-            const url = 'https://gnews.io/api/v4/search?q='+ search +'&apikey='+ token;
-            
-            try {
-                const response = await fetch(url);
-                const data = await response.json();
-                this.setState({totalArticles: data.articles.length, articles: data.articles});
-            } catch (error) {
-                console.error('Error:', error);
-            }
+        if(search.length == 0){
+            console.log("No hay palabras");
+            const inputElement = document.getElementById('searchId');
+            alertCtrlElement.classList.remove('d-none');        
+            inputElement.focus();
+            return;
         }
+
+        const token = '4588013a57a1fc2bb9ef42f2bec8f459';
+        const url = 'https://gnews.io/api/v4/search?q='+ search +'&apikey='+ token;
         
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            this.setState({totalArticles: data.articles.length, articles: data.articles});
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 
     handleInputChange = (event) => {
         this.setState({ search: event.target.value });
+    };
+
+    handleKeyDown = (event) => {
+        if(event.key == 'Enter'){
+            this.searchNews();
+        }
+        else {
+            document.getElementById('alertCtrl').classList.add('d-none');
+        }
     };
 
     render(){
@@ -43,13 +58,28 @@ class Searcher extends React.Component {
                         <img src={logo} className="img-fluid" alt="logo" />
                     </div>
                     <div className='col-8'>
-                        <input type='text' className='form-control' value={this.state.search} onChange={this.handleInputChange}/>
+                        <input type='text' 
+                            id='searchId'
+                            className='form-control' 
+                            value={this.state.search} 
+                            onChange={this.handleInputChange} 
+                            onKeyDown={this.handleKeyDown}/>
                     </div>
                     <div className='col-2'>
                         <button type='button' className='btn btn-search-orange' onClick={this.searchNews}>Search</button>
                     </div>
                 </div>
+                
+                <div className='row mt-2'>
+                    <div className='col-8 offset-2'>
+                        <div id='alertCtrl' className="alert alert-danger d-none" role="alert">
+                            Favor de indicar que noticia y/o artículo desea buscar
+                        </div>
+                    </div>
+                </div>
+
                 <hr/>
+
                 <div className='row'>
                     <p>Total Articles: {this.state.totalArticles}</p>
 
